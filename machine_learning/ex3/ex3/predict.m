6a24a2d949a618d948126a32eeb24b2e1f9d9fd0
Shengzhe Yao <yaoshengzhe@gmail.com>
2012-09-05 00:12:42 -0700

    finish machine learning ex3

--- machine_learning/ex3/ex3/predict.m
+++ machine_learning/ex3/ex3/predict.m
@@ -21,7 +21,10 @@ p = zeros(size(X, 1), 1);
 %       can use max(A, [], 2) to obtain the max for each row.
 %
 
+a2 = sigmoid([ones(m, 1), X] * Theta1');
+a3 = sigmoid([ones(size(a2, 1), 1), a2] * Theta2');
 
+[val, p] = max(a3, [], 2);
 
 
 
--- machine_learning/ex3/ex3/predictOneVsAll.m
+++ machine_learning/ex3/ex3/predictOneVsAll.m
@@ -30,7 +30,7 @@ X = [ones(m, 1) X];
 %       for each row.
 %       
 
-
+[val, p] = max(sigmoid(X * all_theta'), [], 2);
 
 
 
